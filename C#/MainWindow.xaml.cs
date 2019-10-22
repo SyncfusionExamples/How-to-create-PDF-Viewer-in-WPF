f81419447406ff92bb50da270c1dca98226f4c23
Ramkumar Ravy <ramkumarr@syncfusion.com>
2019-10-22 15:18:59 +0530

    Added samples for how to create PDF Viewer in C# and VB.Net WPF

---
+++ C#/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace CreatePdfViewerControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            pdfViewerControl.Load(@"../../Data/Barcode.pdf");
+        }
+    }
+}
---
+++ VB.Net/MainWindow.xaml.vb
@@ -0,0 +1,5 @@
+﻿Class MainWindow
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        PdfViewerControl.Load("../../Data/Barcode.pdf")
+    End Sub
+End Class
